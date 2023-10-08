74b2745416d252103969bcb61e410b227378b7fa
jeanvips <jeanvips12345678@gmail.com>
2023-10-09 00:03:30 +0300

    Задача 9-3

---
+++ TeachPython/Lesson9/Task9_3.py
@@ -0,0 +1,8 @@
+import collections
+f = open("text_01.txt", encoding ="utf-8")
+s0 = f.read()
+s0 = s0.lower()
+s0 = sorted(dict(collections.Counter(s0)).items(), key = lambda x: (-x[1], x[0]))
+for i, j in s0[:10]:
+    print(i, "–", j)
+f.close()

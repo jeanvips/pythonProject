bf959b58fad05f979b001a4efe975d3923f8143c
jeanvips <jeanvips12345678@gmail.com>
2023-10-22 19:33:17 +0300

    Задача 15-2

--- TeachPython/lesson15/Task15_2.py
+++ TeachPython/lesson15/Task15_2.py
@@ -4,4 +4,4 @@ def car_num(s):
     regex = r"\b[ABCEHKMOPTXАВЕКМНОРСТХ]\d{3}[ABCEHKMOPTXАВЕКМНОРСТХ]{2}78|\b[ABCEHKMOPTXАВЕКМНОРСТХ]\d{3}[ABCEHKMOPTXАВЕКМНОРСТХ]{2}178"
     return re.findall(regex, s)
 
-print(car_num(input("Введите номера автомобилей: ")))
+print(car_num(input()))
